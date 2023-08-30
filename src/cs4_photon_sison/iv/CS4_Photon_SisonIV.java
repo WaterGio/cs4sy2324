b4b27ed478495200681e5979905181a19aa427db
PHOTON <PHOTON@172.22.16.100>
2023-08-30 16:03:42 +0800

    Java Project 1

---
+++ src/cs4_photon_sison/iv/CS4_Photon_SisonIV.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs4_photon_sison.iv;
+
+/**
+ *
+ * @author PHOTON
+ */
+public class CS4_Photon_SisonIV {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String noanswer = "Hello World!";
+        System.out.printIn(noanswer + "Hello Pisay!");
+    }
+    
+}
